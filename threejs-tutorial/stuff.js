9870c5cd8da988f3d8ecd9826b2ca2478d36d7e0
Leon Cheng <leonc@mit.edu>
2021-04-08 01:06:27 -0400

    basic cube working

--- threejs-tutorial/stuff.js
+++ threejs-tutorial/stuff.js
@@ -1,1 +1,33 @@
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+
+
+
+function main() {
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({canvas});
+    
+    const fov = 75;
+    const aspect = 2;
+    const near = 0.1;
+    const far = 5;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    
+    camera.position.z = 2;
+    
+    const scene = new THREE.Scene();
+    
+    const [boxWidth, boxHeight, boxDepth] = [1, 1, 1];
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+    const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
+    
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    
+    renderer.render(scene, camera);
+}
+
+main();
+
+
+
 console.log("hi there");
