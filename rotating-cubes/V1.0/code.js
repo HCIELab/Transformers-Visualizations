2e7c6dab53fb864b23fdd2797aaf592ad62272e2
Leon Cheng <leonc@mit.edu>
2021-04-12 23:52:55 -0400

    rotate cube around edge demo

--- rotating-cubes/V1.0/code.js
+++ rotating-cubes/V1.0/code.js
@@ -9,7 +9,7 @@ function main() {
     const fov = 75;
     const aspect = window.innerWidth / window.innerHeight;
     const near = 0.1;
-    const far = 5;
+    const far = 50;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.z = 4;
 
@@ -35,28 +35,33 @@ function main() {
     // Geometry, Material, Mesh
     const [boxWidth, boxHeight, boxDepth] = [1, 1, 1];
     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+    geometry.translate( boxWidth/ 2, boxHeight / 2, boxDepth / 2 ); 
     
+    const cubes = [];
     function makeInstance(geometry, color, x) {
-        [THREE.BackSide, THREE.FrontSide].forEach((side) => {
-            const material = new THREE.MeshPhongMaterial({
-                color,
-                opacity: 0.5,
-                transparent: true,
-                side,
-            });
-            const cube = new THREE.Mesh(geometry, material);
-            scene.add(cube);
-            cube.position.x = x;
-        });
+      [THREE.BackSide, THREE.FrontSide].forEach((side) => {
+          const material = new THREE.MeshPhongMaterial({
+              color,
+              opacity: 0.5,
+              transparent: true,
+              side,
+          });
+          const cube = new THREE.Mesh(geometry, material);
+          scene.add(cube);
+          cube.position.x = x;
+          cubes.push(cube);
+      });
     }
     makeInstance(geometry, 0x44aa88, 0);
     makeInstance(geometry, 0xcc0000, 1.1);
+    // console.log(cubes);
+
 
     // Add a revolving animation
     function addRevolveAnimation(mesh, timeSeconds) {
         const speed = 1;
         const rot = timeSeconds * speed;
-        mesh.rotation.x = rot;
+        // mesh.rotation.x = rot;
         mesh.rotation.y = rot;
     }
 
@@ -82,7 +87,9 @@ function main() {
             controls.update();
         }
 
-        // addRevolveAnimation(cube, time*0.001);
+        cubes.map((c) => {
+          addRevolveAnimation(c, time*0.001);
+        })
 
         renderer.render(scene, camera);
         requestAnimationFrame(render);
