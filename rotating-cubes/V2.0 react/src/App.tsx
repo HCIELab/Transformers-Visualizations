004f926e0dac98d9d507bd3750f697641de6c1c1
Leon Cheng <leonc@mit.edu>
2021-04-24 13:55:09 -0400

    add a section of area above the canvas

--- rotating-cubes/V2.0 react/src/App.tsx
+++ rotating-cubes/V2.0 react/src/App.tsx
@@ -3,11 +3,9 @@ import styled from 'styled-components';
 import Demo from "./Demo";
 
 const AppContainer = styled.div`
-	padding: 50px;
 	width: 100vw;
 	height: 100vh;
 	overflow: auto;
-	background-color: gray;
 `;
 
 const App = () => {
--- rotating-cubes/V2.0 react/src/Demo/index.tsx
+++ rotating-cubes/V2.0 react/src/Demo/index.tsx
@@ -1,20 +1,52 @@
 import React from 'react';
 
+import styled from 'styled-components';
 import { Canvas } from '@react-three/fiber';
 
 import Cube from "../Cube";
 import Controls from "../Controls";
 
+
+const DemoContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    border: 25px solid #000000;
+
+    .TopSection {
+        margin: 0;
+        height: 20%;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .CanvasContainer {
+        margin: 0;
+        height: 80%;
+        width: 100%;
+        background-color: gray;
+    }
+`;
+
 const Demo = () => {
 
     return (
-        <Canvas>
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
-            <Controls/>
-            <Cube position={[0, 0, 0]} color={"#000000"}/>
-            <Cube position={[1, 0, 0]} color={"#000000"}/>
-        </Canvas>
+        <DemoContainer>
+            <div className="TopSection">
+                (add stuff here...)
+            </div>
+            <div className="CanvasContainer">
+                <Canvas>
+                    <ambientLight />
+                    <pointLight position={[10, 10, 10]} />
+                    <Controls/>
+                    <Cube position={[0, 0, 0]} color={"#000000"}/>
+                    <Cube position={[1, 0, 0]} color={"#000000"}/>
+                </Canvas>
+            </div>
+        </DemoContainer>
     )
 }
 
