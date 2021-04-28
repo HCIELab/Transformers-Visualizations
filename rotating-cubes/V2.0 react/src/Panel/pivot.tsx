72c5e79df29ee1713ac93f63ccda4f619c0dce24
Leon Cheng <leonc@mit.edu>
2021-04-27 23:55:48 -0400

    add clickable corners

--- rotating-cubes/V2.0 react/src/Panel/pivot.tsx
+++ rotating-cubes/V2.0 react/src/Panel/pivot.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styled from 'styled-components';
 
-const PivotContainer = styled.div`
+const PivotContainer = styled.div<{corner: cornerType}>`
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -10,23 +10,68 @@ const PivotContainer = styled.div`
     padding: 20px;
 
     .MannequinSquare {
+        position: relative;
         width: 100px;
         height: 100px;
         background-color: #eeeeee;
-        border: 5px solid black;
+        border: 3px solid black;
+
+        .Corner {
+            position: absolute;
+            background-color: teal;
+            opacity: 0.5;
+            width: 20px;
+            height: 20px;
+            cursor: pointer;
+            border-radius: 100%;
+        }
+        .NorthEast { 
+            top: -10px;
+            right: -10px;
+        }
+        .SouthEast { 
+            bottom: -10px;
+            right: -10px;
+        }
+        .SouthWest { 
+            bottom: -10px;
+            left: -10px;
+        }
+        .NorthWest { 
+            top: -10px;
+            left: -10px;
+        }
+        ${props => `
+            .${props.corner} {
+                background-color: red;
+            }
+        `}
     }
 `;
 
+type cornerType = "NorthEast" | "SouthEast" | "SouthWest" | "NorthWest";
+
 const Pivot = (props: {
 }) => {
+
+    const [corner, setCorner] = useState<cornerType>("NorthEast");
+
+    const cornerClick = (corner: cornerType) => {
+        setCorner(corner);
+    }
     
     return (
-        <PivotContainer>
+        <PivotContainer corner={corner}>
             <p>
                 Choose your pivot point here:
             </p>
 
-            <div className="MannequinSquare"></div>
+            <div className="MannequinSquare">
+                <div className="NorthEast Corner" onClick={() => cornerClick("NorthEast")}></div>
+                <div className="SouthEast Corner" onClick={() => cornerClick("SouthEast")}></div>
+                <div className="SouthWest Corner" onClick={() => cornerClick("SouthWest")}></div>
+                <div className="NorthWest Corner" onClick={() => cornerClick("NorthWest")}></div>
+            </div>
 
         </PivotContainer>
     )
