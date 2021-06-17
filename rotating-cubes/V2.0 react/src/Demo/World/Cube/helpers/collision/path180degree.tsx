f03639e6bafd6407f327bf68f20acebdb404cb5d
Leon Cheng <leonc@mit.edu>
2021-06-17 16:31:19 -0400

    a couple more configs in path180degree.tsx

--- rotating-cubes/V2.0 react/src/Demo/World/Cube/helpers/collision/path180degree.tsx
+++ rotating-cubes/V2.0 react/src/Demo/World/Cube/helpers/collision/path180degree.tsx
@@ -19,16 +19,14 @@ export const traversedPath : pathReturnType = {
             new Vector2(a+2, b),
             new Vector2(a+2, b+1),
             new Vector2(a+1, b+1),
-        ]
-        ,
+        ],
         CLOCKWISE: (a, b) => [
-            // TODO: fix these coords
-            new Vector2(a, b+1),
-            new Vector2(a-1, b+1),
             new Vector2(a-1, b),
-            new Vector2(a-2, b),
-            new Vector2(a-1, b-1),
-            new Vector2(a-2, b-1),
+            new Vector2(a-1, b+1),
+            new Vector2(a, b+1),
+            new Vector2(a, b+2),
+            new Vector2(a+1, b+2),
+            new Vector2(a+1, b+1),
         ],
     },
     SouthEast: {
@@ -82,13 +80,12 @@ export const traversedPath : pathReturnType = {
             new Vector2(a+1, a),
         ],
         CLOCKWISE: (a, b) => [
-            // TODO: fix these coords
-            new Vector2(a+1, b),
-            new Vector2(a+1, b+1),
-            new Vector2(a, a+1),
-            new Vector2(a, a+2),
-            new Vector2(a-1, a+1),
-            new Vector2(a-1, a+2),
+            new Vector2(a, b-1),
+            new Vector2(a-1, b-1),
+            new Vector2(a-1, b),
+            new Vector2(a-2, b),
+            new Vector2(a-2, b+1),
+            new Vector2(a-1, b+1),
         ],
     }
 }
