2da0d2788aef3008bb22b2e212852f18410a1c1e
Leon Cheng <leonc@mit.edu>
2021-06-28 16:36:11 -0400

    fix test cases in getPointOfRotation

--- rotating-cubes/V2.0 react/src/Demo/World/Cube/helpers/getPointOfRotation.test.tsx
+++ rotating-cubes/V2.0 react/src/Demo/World/Cube/helpers/getPointOfRotation.test.tsx
@@ -1,13 +1,13 @@
-import { Euler, Vector3 } from "three"
+import { Euler, Quaternion, Vector3 } from "three"
 import {getPointOfRotation} from "./getPointOfRotation"
 
 test('corner: NorthEast, axis: z, rotation: (0, 0, 0)', async () => {
-    const point = getPointOfRotation("NorthEast", 1, "z", new Euler(0, 0, 0));
+    const point = getPointOfRotation("NorthEast", 1, "z", new Quaternion().setFromEuler( new Euler(0, 0, 0) ));
     expect(point).toEqual(new Vector3(0.5, 0.5, 0));
 })
 
 test('corner: NorthEast, axis: z, rotation: (0, 0, Math.PI)', async () => {
-    const point = getPointOfRotation("NorthEast", 1, "z", new Euler(0, 0, Math.PI));
+    const point = getPointOfRotation("NorthEast", 1, "z", new Quaternion().setFromEuler( new Euler(0, 0, Math.PI)) );
     expect(point).toEqual(new Vector3(-0.5, -0.5, 0));
 })
 
@@ -15,28 +15,28 @@ test('corner: NorthEast, axis: z, rotation: (0, 0, Math.PI)', async () => {
 
 
 test('corner: NorthEast, axis: x, rotation: (0, 0, 0)', async () => {
-    const point = getPointOfRotation("NorthEast", 1, "x", new Euler(0, 0, 0));
+    const point = getPointOfRotation("NorthEast", 1, "x", new Quaternion().setFromEuler( new Euler(0, 0, 0)) );
     expect(point).toEqual(new Vector3(0, 0.5, -0.5));
 })
 
 test('corner: NorthEast, axis: x, rotation: (0, 0, -2*Math.PI)', async () => {
-    const point = getPointOfRotation("NorthEast", 1, "x", new Euler(0, 0, -2*Math.PI));
-    expect(point).toEqual(new Vector3(-0, 0.5, -0.5));
+    const point = getPointOfRotation("NorthEast", 1, "x", new Quaternion().setFromEuler( new Euler(0, 0, -2*Math.PI)) );
+    expect(point).toEqual(new Vector3(0, 0.5, -0.5));
 })
 
 test('corner: NorthEast, axis: x, rotation: (0, 0, -Math.PI)', async () => {
-    const point = getPointOfRotation("NorthEast", 1, "x", new Euler(0, 0, -Math.PI));
-    expect(point).toEqual(new Vector3(0, -0.5, -0.5));
+    const point = getPointOfRotation("NorthEast", 1, "x", new Quaternion().setFromEuler( new Euler(0, 0, -Math.PI)) );
+    expect(point).toEqual(new Vector3(-0, -0.5, -0.5));
 })
 
 
 
 test('corner: NorthEast, axis: y, rotation: (0, 0, 0)', async () => {
-    const point = getPointOfRotation("NorthEast", 1, "y", new Euler(0, 0, 0));
+    const point = getPointOfRotation("NorthEast", 1, "y", new Quaternion().setFromEuler( new Euler(0, 0, 0)) );
     expect(point).toEqual(new Vector3(0.5, 0, -0.5));
 })
 
 test('corner: NorthEast, axis: y, rotation: (-Math.PI, 0, -Math.PI)', async () => {
-    const point = getPointOfRotation("NorthEast", 1, "y", new Euler(-Math.PI, 0, -Math.PI));
+    const point = getPointOfRotation("NorthEast", 1, "y", new Quaternion().setFromEuler( new Euler(-Math.PI, 0, -Math.PI)) );
     expect(point).toEqual(new Vector3(-0.5, 0, 0.5));
 })
