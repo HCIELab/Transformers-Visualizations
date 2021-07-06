0634aaee84cce158d93223b3e99df0601636ed36
Leon Cheng <leonc@mit.edu>
2021-07-06 16:45:34 -0400

    keep figure code minimal besides ref manipulation

--- V4.0 react-bare/src/Demo/World/Figures/Box/index.tsx
+++ V4.0 react-bare/src/Demo/World/Figures/Box/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import { Color } from "@react-three/fiber";
 
@@ -6,30 +6,47 @@ import STLHelper from "./STLhelper";
 import { Vector3 } from 'three';
 
 const Box = React.forwardRef<THREE.Group, {
+    id: number,
     color: Color,
     position: Vector3,
 }>( (props, ref) => {
 	const side = 1;
 
 	return (
-		<group
-            ref={ref}
-            position={props.position}
+        <Suspense 
+            fallback={<FallbackBox/>}
+            key={props.id}
         >
-            <STLHelper
-                side={side}
-                color={props.color}
-                filepath={"stl-assets/assem-Simul-coils.STL"}
-            />
-            <STLHelper
-                side={side}
-                color={"#007e9e"} // Color of the corners. Change this to a prop if you want greater customization over this color
-                filepath={"stl-assets/assem-Simul-corners.STL"}
-            />
-		</group>
+
+            <group
+                ref={ref}
+                position={props.position}
+            >
+                <STLHelper
+                    side={side}
+                    color={props.color}
+                    filepath={"stl-assets/assem-Simul-coils.STL"}
+                />
+                <STLHelper
+                    side={side}
+                    color={"#007e9e"} // Color of the corners. Change this to a prop if you want greater customization over this color
+                    filepath={"stl-assets/assem-Simul-corners.STL"}
+                />
+            </group>
+        </Suspense>
 	)
 })
 
+
+
+const FallbackBox = () => {
+    return (
+        <mesh>
+            <boxGeometry/>
+            <meshStandardMaterial color={"gray"}/>
+        </mesh>
+    ) 
+}
 
 
 export default Box;
--- V4.0 react-bare/src/Demo/World/Figures/index.tsx
+++ V4.0 react-bare/src/Demo/World/Figures/index.tsx
@@ -1,45 +1,19 @@
-import React, { Suspense, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Vector3 } from 'three';
-
-import { initialCubeConfigType } from '../../Util/Types/types';
 
 import Box from "./Box";
 
-const FallbackBox = () => {
-    return (
-        <mesh>
-            <boxGeometry/>
-            <meshStandardMaterial color={"gray"}/>
-        </mesh>
-    ) 
-}
-
-
-
-
-
 export const Figure1 = (props: {
 }) => {
-
-    const initialConfig : initialCubeConfigType = {
-        id: 1,
-        initialPosition: new Vector3(0, 0, 0),
-        color: "#ffffff",
-    }
-    
     const ref = useRef<THREE.Group>(null!);
 
     return (
-        <Suspense 
-            fallback={<FallbackBox/>}
-            key={initialConfig.id}
-        >
-            <Box
-                ref={ref}
-                position={initialConfig.initialPosition}
-                color={initialConfig.color}
-            />
-        </Suspense>        
+        <Box
+            ref={ref}
+            id={1}
+            position={new Vector3(0, 0, 0)}
+            color={"#ffffff"}
+        />
     )
 }
 
@@ -47,14 +21,7 @@ export const Figure1 = (props: {
 
 export const Figure2 = (props: {
 }) => {
-    const initialConfig : initialCubeConfigType = {
-        id: 1,
-        initialPosition: new Vector3(1, 0, 0),
-        color: "#00f535",
-    }
-
     const ref = useRef<THREE.Group>(null!);
-
     useEffect(() => {
         setTimeout(() => {
             ref.current.position.set(0, 1, 0);
@@ -62,16 +29,12 @@ export const Figure2 = (props: {
     }, [])
     
     return (
-        <Suspense 
-            fallback={<FallbackBox/>}
-            key={initialConfig.id}
-        >
-            <Box
-                ref={ref}
-                position={initialConfig.initialPosition}
-                color={initialConfig.color}
-            />
-        </Suspense>        
+        <Box
+            ref={ref}
+            id={2}
+            position={new Vector3(1, 0, 0)}
+            color={"#00f535"}
+        />
     )
 }
 
