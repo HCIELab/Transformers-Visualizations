567fef783cda93ccfb8c8ded75d19c6d11e0be43
Leon Cheng <leonc@mit.edu>
2021-07-10 18:45:14 -0400

    WIP: positions for chair

--- V2.0 react/src/Demo/Instructions/button10.tsx
+++ V2.0 react/src/Demo/Instructions/button10.tsx
@@ -6,8 +6,34 @@ export const Button10 = (props: {
 }) => (
     <button onClick={() => {
         props.setInitialCubeConfigs([
-            {id: 1, initialPosition: new Vector3(1, 0, 0), color: "#77410e"},
-            {id: 2, initialPosition: new Vector3(0, 0, 0), color: "#77410e"},            
+            // Chair left side
+            {id: 1, initialPosition: new Vector3(0, 0, 0), color: "#77410e"},
+            {id: 2, initialPosition: new Vector3(0, 1, 0), color: "#77410e"},            
+            {id: 3, initialPosition: new Vector3(0, 2, 0), color: "#77410e"},            
+            {id: 4, initialPosition: new Vector3(0, 3, 0), color: "#77410e"},            
+            
+            {id: 5, initialPosition: new Vector3(0, 1, 1), color: "#77410e"},            
+            {id: 6, initialPosition: new Vector3(0, 1, 2), color: "#77410e"},            
+            {id: 7, initialPosition: new Vector3(0, 0, 2), color: "#77410e"},            
+
+
+            // Chair right side
+            {id: 8, initialPosition: new Vector3(2, 0, 0), color: "#77410e"},
+            {id: 9, initialPosition: new Vector3(2, 1, 0), color: "#77410e"},            
+            {id: 10, initialPosition: new Vector3(2, 2, 0), color: "#77410e"},            
+            {id: 11, initialPosition: new Vector3(2, 3, 0), color: "#77410e"},            
+            
+            {id: 12, initialPosition: new Vector3(2, 1, 1), color: "#77410e"},            
+            {id: 13, initialPosition: new Vector3(2, 1, 2), color: "#77410e"},            
+            {id: 14, initialPosition: new Vector3(2, 0, 2), color: "#77410e"},            
+
+            // Chair middle
+            {id: 15, initialPosition: new Vector3(1, 1, 0), color: "#77410e"},            
+            {id: 16, initialPosition: new Vector3(1, 2, 0), color: "#77410e"},            
+            {id: 17, initialPosition: new Vector3(1, 3, 0), color: "#77410e"},            
+            {id: 18, initialPosition: new Vector3(1, 1, 1), color: "#77410e"},            
+            {id: 19, initialPosition: new Vector3(1, 1, 2), color: "#77410e"},            
+
         ])
         props.setInstructions([
         ])
