35d7a4ecb70e171349551ba0dd3dc51b53ada35f
Leon Cheng <leonc@mit.edu>
2021-07-19 22:58:44 -0400

    WIP: arrows added for all 3 axes, but not yet functional

--- V2.0 react/src/Demo/World/Arrows/index.tsx
+++ V2.0 react/src/Demo/World/Arrows/index.tsx
@@ -9,6 +9,8 @@ const Arrows = (props: {
     return (
         <group position={new Vector3(-2, -1, 0)}>
             <axesHelper/>
+
+            {/* Z axis Arrows */}
             <group scale={0.3} position={new Vector3(0, 0.222, 0.5)}>
                 <STLHelper
                     side={1}
@@ -23,6 +25,39 @@ const Arrows = (props: {
                     filepath={"stl-assets/curved-arrow.stl"}
                 />
             </group>
+
+            {/* Y axis Arrows */}
+            <group scale={0.3} position={new Vector3(0, 0.5, 0.222)} rotation={new Euler(Math.PI/2, 0, 0)}>
+                <STLHelper
+                    side={1}
+                    color={"#24ff2f"}
+                    filepath={"stl-assets/curved-arrow.stl"}
+                />
+            </group>
+            <group scale={0.3} position={new Vector3(0, 0.5, -0.222)} rotation={new Euler(Math.PI/2, 0, Math.PI)}>
+                <STLHelper
+                    side={1}
+                    color={"#24ff2f"}
+                    filepath={"stl-assets/curved-arrow.stl"}
+                />
+            </group>
+
+            {/* X axis Arrows */}
+            <group scale={0.3} position={new Vector3(0.5, 0.222, 0)} rotation={new Euler(0, Math.PI/2, 0)} >
+                <STLHelper
+                    side={1}
+                    color={"#ff640a"}
+                    filepath={"stl-assets/curved-arrow.stl"}
+                />
+            </group>
+            <group scale={0.3} position={new Vector3(0.5, -0.222, 0)} rotation={new Euler(0, Math.PI/2, Math.PI)}>
+                <STLHelper
+                    side={1}
+                    color={"#ff640a"}
+                    filepath={"stl-assets/curved-arrow.stl"}
+                />
+            </group>
+
         </group>
     )
 }
