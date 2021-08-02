b7bedc07f30cce2358eee8a176398e9098f6a813
Leon Cheng <leonc@mit.edu>
2021-08-02 17:33:09 -0400

    z axis 180 degree counterclockwise rotations working

--- V2.0 react/src/Demo/World/Cube/StationaryEmags/locations.tsx
+++ V2.0 react/src/Demo/World/Cube/StationaryEmags/locations.tsx
@@ -7,6 +7,8 @@ export type locationsType = {
     catchingPosition: Vector3,
 }
 
+const pi = Math.PI;
+
 export const getLocations = (cubePosition: Vector3, side: number, offset: number, cornerName: cornerType) : locationsType => {
     const half = side/2;
 
@@ -22,18 +24,34 @@ export const getLocations = (cubePosition: Vector3, side: number, offset: number
         hingePosition: new Vector3(0, 0, 0),
         catchingPosition: new Vector3(0, 0, 0),
     };
+
+    let euler = new Euler(0, 0, 0);
     switch (cornerName) {
         case "NorthEast":
-            // CLOCKWISE: generateTemplateCoordList().map(
-            //     (v) => v.applyEuler(new Euler(0, -pi/2, 3*pi/2))
-            // ),
-            const euler = new Euler(0, 0, 0);
+            euler = new Euler(0, 0, -pi/2);
             adjustment = {
                 repulsionPosition: template.repulsionPosition.applyEuler(euler),
                 hingePosition: template.hingePosition.applyEuler(euler),
                 catchingPosition: template.catchingPosition.applyEuler(euler),
             }
             break;
+        case "SouthEast":
+            euler = new Euler(0, 0, -pi);
+            adjustment = {
+                repulsionPosition: template.repulsionPosition.applyEuler(euler),
+                hingePosition: template.hingePosition.applyEuler(euler),
+                catchingPosition: template.catchingPosition.applyEuler(euler),
+            }
+            break;
+        case "SouthWest":
+            euler = new Euler(0, 0, -3*pi/2);
+            adjustment = {
+                repulsionPosition: template.repulsionPosition.applyEuler(euler),
+                hingePosition: template.hingePosition.applyEuler(euler),
+                catchingPosition: template.catchingPosition.applyEuler(euler),
+            }
+            break;
+        case "NorthWest":
         default:
             adjustment = template;
     }
