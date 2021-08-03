3e5a05c950a0ef1daa69acd21180cf247a837ae2
Leon Cheng <leonc@mit.edu>
2021-08-02 22:58:13 -0400

    WIP: button12 traversal z axis test

---
+++ V2.0 react/src/Demo/Instructions/button12.tsx
@@ -0,0 +1,82 @@
+import { Vector3 } from "three"
+import Button from '@material-ui/core/Button';
+
+export const Button12 = (props: {
+    setInstructions: Function,
+    setInitialCubeConfigs: Function,
+    setIncrementAmount: Function,
+}) => (
+    <Button variant="outlined" color="primary" onClick={() => {
+        props.setIncrementAmount(0.1);
+        props.setInitialCubeConfigs([
+            {id: 1, initialPosition: new Vector3(1, 0, 0), color: "#77410e"},
+            {id: 2, initialPosition: new Vector3(0, 0, 0), color: "#77410e"},            
+            {id: 3, initialPosition: new Vector3(0, 1, 0), color: "#77410e"},         
+            {id: 4, initialPosition: new Vector3(-1, 0, 0), color: "#77410e"},         
+            {id: 5, initialPosition: new Vector3(-1, 1, 0), color: "#77410e"},         
+            {id: 6, initialPosition: new Vector3(-1, 2, 0), color: "#77410e"},         
+            {id: 7, initialPosition: new Vector3(-1, 3, 0), color: "#77410e"},         
+            {id: 8, initialPosition: new Vector3(0, 3, 0), color: "#77410e"},         
+            {id: 9, initialPosition: new Vector3(1, 3, 0), color: "#77410e"},         
+            {id: 10, initialPosition: new Vector3(2, 3, 0), color: "#77410e"},         
+            {id: 11, initialPosition: new Vector3(2, 2, 0), color: "#77410e"},         
+            {id: 12, initialPosition: new Vector3(2, 1, 0), color: "#77410e"},         
+            {id: 13, initialPosition: new Vector3(2, 0, 0), color: "#77410e"},         
+               
+        ])
+        props.setInstructions([
+            {
+                cubeID: 3,
+                axis: "z",
+                isCounterclockwise: true,
+                timeToStart: 1000,
+            },
+            {
+                cubeID: 3,
+                axis: "z",
+                isCounterclockwise: true,
+                timeToStart: 2000,
+            },
+            {
+                cubeID: 3,
+                axis: "z",
+                isCounterclockwise: true,
+                timeToStart: 3000,
+            },
+            {
+                cubeID: 3,
+                axis: "z",
+                isCounterclockwise: true,
+                timeToStart: 4000,
+            },
+
+
+            {
+                cubeID: 3,
+                axis: "z",
+                isCounterclockwise: false,
+                timeToStart: 5000,
+            },
+            {
+                cubeID: 3,
+                axis: "z",
+                isCounterclockwise: false,
+                timeToStart: 6000,
+            },
+            {
+                cubeID: 3,
+                axis: "z",
+                isCounterclockwise: false,
+                timeToStart: 7000,
+            },
+            {
+                cubeID: 3,
+                axis: "z",
+                isCounterclockwise: false,
+                timeToStart: 8000,
+            },
+        ])
+    }}>
+        Traversal Z axis test
+    </Button>
+)
